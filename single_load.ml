5e468a858b1a6d36b923fa82dadddbcf03c85a53
Allan Blanchard <allan.blanchard0@gmail.com>
2017-06-22 16:15:36 +0200

    Single loads now cleaner copy + inplace -> copy

--- single_load.ml
+++ single_load.ml
@@ -12,70 +12,90 @@ class name_finder = object(_)
     match lv with Var(vi), _ -> raise (Name vi.vname) | _ -> Cil.DoChildren
 end
 
+let find_name lv = try
+    ignore Visitor.(visitFramacLval (new name_finder :> inplace) lv) ; "aux"
+  with (Name s) -> s
+
 class visitor prj = object(me)
   inherit copy prj
 
   method private pr_vexp  e  = Visitor.visitFramacExpr (me :> copy) e
   method private pr_vlval lv = Visitor.visitFramacLval (me :> copy) lv
   method private pr_voff  o  = Visitor.visitFramacOffset (me :> copy) o
-
+      
+  method private pr_nvi vi = Cil.memo_varinfo me#behavior vi
+      
   val mutable insert_ph = false
 
   method! vfunc _ =
     Cil.DoChildrenPost
       (fun f -> Cfg.clearCFGinfo ~clear_id:false f ; Cfg.cfgFun f ; f)
 
-  method! vstmt_aux _ =
+  method! vstmt_aux s =
     if insert_ph then begin
-      insert_ph <- false ;
-      me#queueInstr [Cil.dummyInstr] ;
-      Options.Self.feedback "Inserting a dummy instruction"
+      insert_ph <- false ; me#queueInstr [Cil.dummyInstr]
     end ;
-    Cil.DoChildren
+    match s.skind with
+    | Block(_) when Atomic_spec.atomic_stmt(s) -> Cil.JustCopy
+    | _ -> Cil.DoChildren
 
   method! vinst inst =
-    begin match inst with
-      | Local_init(_, ConsInit(_, _, _), _) ->
-        insert_ph <- not (Atomic_spec.atomic_call inst)
-      | Call(Some(_), _, _, _) ->
-        insert_ph <- not (Atomic_spec.atomic_call inst)
-      | _ -> ()
-    end ; Cil.DoChildren
+    let replace lv =
+      match lv with
+      | Var(vi), o -> Var(me#pr_nvi vi), me#pr_voff o
+      | Mem(e), o  -> Mem(me#pr_vexp e), me#pr_voff o
+    in
+    
+    match inst with
+    | Local_init(vi, ConsInit(fvi, l, k), loc) ->
+      insert_ph <- not (Atomic_spec.atomic_call inst) ;
+      let nvi  = me#pr_nvi vi in
+      let nfvi = me#pr_nvi fvi in
+      let nl   = List.map (me#pr_vexp) l in
+      Cil.ChangeTo([Local_init(nvi, ConsInit(nfvi, nl, k), loc)])
+    | Call(Some(lv), efct, l, loc) ->
+      let nlv = replace lv in
+      let nf = match efct.enode with
+        | Lval(Var(fct), NoOffset) ->
+          Cil.new_exp ~loc (Lval(Cil.var (me#pr_nvi fct)))
+        | _ ->
+          raise (Errors.BadConstruct "function pointers")
+      in
+      let nl = List.map (me#pr_vexp) l in
+      insert_ph <- not (Atomic_spec.atomic_call inst) ;
+      Cil.ChangeTo([Call(Some(nlv), nf, nl, loc)])
+    | Set(lv, e, loc) ->
+      Cil.ChangeTo([Set( (replace lv), (me#pr_vexp e), loc)])
+    | _ ->
+      Cil.DoChildren
 
 
   method! vexpr exp =
+    let open Cil in
     let loc = Cil.CurrentLoc.get() in
     match exp.enode with
-    | AddrOf(Var(vi), offset) ->
-      let noffset = me#pr_voff(offset) in
-      let nvi = Cil.memo_varinfo me#behavior vi in
-      Cil.ChangeTo(Cil.new_exp ~loc:loc (AddrOf (Var(nvi), noffset)))
-    | StartOf(Var(vi), offset) ->
-      let noffset = me#pr_voff(offset) in
-      let nvi = Cil.memo_varinfo me#behavior vi in
-      Cil.ChangeTo(Cil.new_exp ~loc:loc (StartOf (Var(nvi), noffset)))
-    | _ -> Cil.DoChildren
+    | AddrOf(Var(vi), o) ->
+      Cil.ChangeTo(new_exp ~loc (AddrOf (Var(me#pr_nvi vi), me#pr_voff o)))
+    | StartOf(Var(vi), o) ->
+      Cil.ChangeTo(new_exp ~loc (StartOf (Var(me#pr_nvi vi), me#pr_voff o)))
+    | _ ->
+      Cil.DoChildren
 
 
-  method! vlval lv =
+  method! vlval _ =
     let loc = Cil.CurrentLoc.get() in
-    let f = match me#current_func with Some(f) -> f | None -> assert false in
-    let nf = Cil.memo_fundec me#behavior f in
+    let nf = match me#current_func with
+    | Some(f) -> Cil.memo_fundec me#behavior f
+    | None -> assert false
+    in
 
     (* Here the lval must be a load in a function *)
-    let load name id lv =
-      let name = name ^ (string_of_int id) in
-      let aux_vi = Cil.makeTempVar nf ~name:name (Cil.typeOfLval lv) in
-      let aux_lv = Cil.var aux_vi in
-      let aux_set = Set(aux_lv, (Cil.new_exp ~loc:loc (Lval lv)), loc) in
-      me#queueInstr [aux_set] ;
-      aux_lv
+    let load id lv =
+      let name = (find_name lv) ^ (string_of_int id) in
+      let aux = Cil.var (Cil.makeTempVar nf ~name (Cil.typeOfLval lv)) in
+      me#queueInstr [ Set(aux, (Cil.new_exp ~loc (Lval lv)), loc) ] ;
+      aux
     in
-    let name = try
-        let _ = Visitor.visitFramacLval (new name_finder :> inplace) lv in "aux"
-      with (Name s) -> s
-    in
-
     let modify lv =
       match me#current_stmt with
       | None -> lv
@@ -83,8 +103,8 @@ class visitor prj = object(me)
 	begin
 	  match lv with
 	  | Var(v), NoOffset | Var(v), Field(_) when v.vformal -> lv 
-	  | Var(v), _ when v.vglob || v.vformal -> load name stmt.sid lv
-	  | Mem(_), _ -> load name stmt.sid lv
+	  | Var(v), _ when v.vglob || v.vformal -> load stmt.sid lv
+	  | Mem(_), _ -> load stmt.sid lv
 	  | _ -> lv
 	end
     in Cil.DoChildrenPost modify
